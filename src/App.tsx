bc471eb66ed2cd19dc478772c18142d6df0f3d69
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-08-08 17:24:03 -0400

    Installed axios to fetch data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 export function App() {
-  return <div>Hello, World </div>
+  const [coins, setCoins] = useState([])
+
+  function LoadAllData() {
+    async function fetchListOfCoins() {
+      const response = await axios.get('https://api.coincap.io/v2/assets')
+    }
+  }
+
+  return (
+    <div>
+      <p>Hello world.</p>
+    </div>
+  )
 }
