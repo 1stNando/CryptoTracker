106f90b6577c42ba245ba0042ec59202262996aa
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-08-08 19:37:12 -0400

    Implemented setInterval() method to manage the frequency of fetching data. Rate of about 10 seconds.

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,27 @@ import axios from 'axios'
 type CoinType = {
   id: string
   rank: number
-  symbol: string
+  symbol: string | null
+  name: string | null
+  priceUsd: number
 }
 
 export function App() {
   //  Teaches TypeScript the shape of our API.
   const [coins, setCoins] = useState<CoinType[]>([])
 
-  function LoadAllData() {
+  // Add STATE for changes in cycles or fetching.
+  const [cycles, setCycles] = useState<number>(0)
+
+  function loadAllData() {
     async function fetchListOfCoins() {
       const response = await axios.get('https://api.coincap.io/v2/assets')
 
       if (response.status === 200) {
-        // Data.data was needed
+        // Data.data was needed to not have error with single .data.
         setCoins(response.data.data)
       }
+      // Just a check to visualize response in browser's console.
       console.log(response.data)
     }
 
@@ -26,14 +32,15 @@ export function App() {
   }
 
   // useEffect has a non-async function, Loads our data ONCE.
-  useEffect(LoadAllData, [])
+  useEffect(loadAllData, [cycles])
 
   // Set interval for reloading data. This discusses useEffect with a return that is a teardown.
   useEffect(function () {
     // If useEffect sets up something that consumes a resource, like an interval identifier...
     const timerInterval = setInterval(function () {
       console.log('Calling my interval')
-    }, 1000)
+      setCycles((cycles) => cycles + 1)
+    }, 10000)
 
     // .. then we need a teardown function to undo that resource. GOOD PRACTICES.
     function teardown() {
@@ -50,7 +57,7 @@ export function App() {
       </header>
       <ul>
         {coins
-          .filter((coin) => coin.rank <= 10)
+          .filter((coin) => coin.rank <= 15)
           .map(function (coin) {
             return (
               <li key={coin.id}>
