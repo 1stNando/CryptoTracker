28a47b841027a5d2ca30f81a914fa4a24628ea1f
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-08-08 19:18:06 -0400

    Corrected error in response. Inside the fetchListOfCoins function, after successfully fetching the data, you are trying to directly set the coins state to the response data, which is not an array of CoinType. You need to extract the relevant data from the response before setting the state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,62 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+type CoinType = {
+  id: string
+  rank: number
+  symbol: string
+}
+
 export function App() {
-  const [coins, setCoins] = useState([])
+  //  Teaches TypeScript the shape of our API.
+  const [coins, setCoins] = useState<CoinType[]>([])
 
   function LoadAllData() {
     async function fetchListOfCoins() {
       const response = await axios.get('https://api.coincap.io/v2/assets')
+
+      if (response.status === 200) {
+        // Data.data was needed
+        setCoins(response.data.data)
+      }
+      console.log(response.data)
     }
+
+    fetchListOfCoins()
   }
 
+  // useEffect has a non-async function, Loads our data ONCE.
+  useEffect(LoadAllData, [])
+
+  // Set interval for reloading data. This discusses useEffect with a return that is a teardown.
+  useEffect(function () {
+    // If useEffect sets up something that consumes a resource, like an interval identifier...
+    const timerInterval = setInterval(function () {
+      console.log('Calling my interval')
+    }, 1000)
+
+    // .. then we need a teardown function to undo that resource. GOOD PRACTICES.
+    function teardown() {
+      clearInterval(timerInterval)
+    }
+
+    return teardown
+  }, [])
+
   return (
-    <div>
-      <p>Hello world.</p>
+    <div className="main">
+      <header>
+        <h1>Hello Crypto.</h1>
+      </header>
+      <ul>
+        {coins.map(function (coin) {
+          return (
+            <li key={coin.id}>
+              {coin.symbol}-{coin.rank}
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
